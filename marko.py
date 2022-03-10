37f94245a1eb5f624cf0308be53579afa2e62a94
MasterMedo <mislav.vuletic@gmail.com>
2022-03-10 18:50:20 +0100

    wrapped

--- marko.py
+++ marko.py
@@ -1,38 +1,45 @@
 from parser import parse, output
 from collections import defaultdict
-import random
-
-f1 = 2
-f2 = 2
-f3 = 5
-def heuristic(demon):
-    return f1*demon.recover_stamina/(demon.recover_turns+1) + f2*sum(demon.fragments)/(1+len(demon.fragments)) - f3*demon.consume_stamina
-path = "./05-androids-armageddon.txt"
-
-stamina, max_stamina, turns, demons = parse("05-androids-armageddon.txt")
-
-detup = []
-for i in range(len(demons)):
-    detup.append((demons[i], i))
-scores = [(heuristic(demon[0]), demon) for demon in detup]
-scores.sort()
+from scoring import score
 
 
-seen = set()
-killed = []
-gain_stamina = defaultdict(int)
-for turn in range(turns):
-    stamina += gain_stamina[turn]
 
-    i = scores[-turn][1][1]
+# Calling example
+# print(main(2, 2, 6, "./05-androids-armageddon.txt"))
 
-    demon = demons[i]
-    if i not in seen and demon.consume_stamina <= stamina:
-        seen.add(i)
-        killed.append(i)
-        stamina -= demon.consume_stamina
-        gain_stamina[turn + demon.recover_turns] += demon.recover_stamina
-    else:
-        turn -= 1
-import scoring
-print(scoring.score(killed, parse(path)))
+# path example
+# path = "./05-androids-armageddon.txt"
+
+
+def main(f1, f2, f3, path):
+    def heuristic(demon):
+        return f1*demon.recover_stamina/(demon.recover_turns+1) + f2*sum(demon.fragments)/(1+len(demon.fragments)) - f3*demon.consume_stamina
+
+
+    stamina, max_stamina, turns, demons = parse(path[2:])
+
+    detup = []
+    for i in range(len(demons)):
+        detup.append((demons[i], i))
+    scores = [(heuristic(demon[0]), demon) for demon in detup]
+    scores.sort()
+
+
+    seen = set()
+    killed = []
+    gain_stamina = defaultdict(int)
+    for turn in range(turns):
+        stamina += gain_stamina[turn]
+
+        i = scores[-turn][1][1]
+
+        demon = demons[i]
+        if i not in seen and demon.consume_stamina <= stamina:
+            seen.add(i)
+            killed.append(i)
+            stamina -= demon.consume_stamina
+            gain_stamina[turn + demon.recover_turns] += demon.recover_stamina
+        else:
+            turn -= 1
+
+    return score(killed, parse(path))
