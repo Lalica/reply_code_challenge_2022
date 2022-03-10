a3a799ed10ed2e9d6e8b335eeb3deab13380340e
MasterMedo <mislav.vuletic@gmail.com>
2022-03-10 18:18:05 +0100

    improvement

--- marko.py
+++ marko.py
@@ -2,26 +2,32 @@ from parser import parse, output
 from collections import defaultdict
 import random
 
-def pick_index(demons, tleft):
-    scores = list()
-    for demon in demons:
-        scores.append(demon.recover_stamina/(demon.recover_turns+1) + sum(demon.fragments[:tleft])/(1+len(demon.fragments[:tleft]))-demon.consume_stamina)
-    return scores.index(max(scores))
+def heuristic(demon):
+    return (demon.recover_stamina/(demon.recover_turns+1) + sum(demon.fragments)/(1+len(demon.fragments))-demon.consume_stamina)
 
-stamina, max_stamina, turns, demons = parse("01-the-cloud-abyss.txt")
+stamina, max_stamina, turns, demons = parse("05-androids-armageddon.txt")
+
+detup = []
+for i in range(len(demons)):
+    detup.append((demons[i], i))
+scores = [(heuristic(demon[0]), demon) for demon in detup]
+scores.sort()
+
 
 seen = set()
 killed = []
 gain_stamina = defaultdict(int)
 for turn in range(turns):
     stamina += gain_stamina[turn]
-    i = pick_index(demons, turns-turn)
+
+    i = scores[-turn][1][1]
+
     demon = demons[i]
     if i not in seen and demon.consume_stamina <= stamina:
         seen.add(i)
         killed.append(i)
         stamina -= demon.consume_stamina
         gain_stamina[turn + demon.recover_turns] += demon.recover_stamina
-    demons.remove(demon)
-
-print(killed)
+    else:
+        turn -= 1
+output(killed)
