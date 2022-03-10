a1cbce31176043cf2a4436f139fca692d214238d
Lalica <lauratoric@gmail.com>
2022-03-10 17:24:36 +0100

    fixed scoring

--- scoring.py
+++ scoring.py
@@ -14,7 +14,6 @@ def score(solution, problem):
     new_demon = 0
     points = 0
     for t in range(turns):
-        print(f"turn {t}")
         for i in range(len(processing_demons)):
             i_d, dt = processing_demons[i]
             demon = demons[i_d]
@@ -23,13 +22,11 @@ def score(solution, problem):
                 stamina += demon.recover_stamina
                 stamina = min(stamina, max_stamina)
 
-        print(f"stamina {stamina}")
-
         if new_demon < len(solution):
             demon = demons[solution[new_demon]]
             if demon.consume_stamina <= stamina:
                 stamina -= demon.consume_stamina
-                processing_demons.append([new_demon, 0])
+                processing_demons.append([solution[new_demon], 0])
                 new_demon += 1
 
         for i in range(len(processing_demons)):
@@ -40,9 +37,6 @@ def score(solution, problem):
                 continue
 
             points += demon.fragments[dt]
-            print(f"new points {demon.fragments[dt]}")
             processing_demons[i][1] += 1
-
-        print(f"points {points}")
 
     return points
